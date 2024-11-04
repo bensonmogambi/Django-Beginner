e7e30b9a127bb452819dc10694976ab3b3fdba06
Benson Mogambi <mogambibenson2@gmail.com>
2024-11-04 12:20:07 +0300

    hey django

---
+++ README.py
@@ -0,0 +1,8 @@
+# commands to start you up on a django project
+#1 - pip install django
+#2 - django-admin startproject 'Project' .    # - Creats the main project, 'Project' this should be the project name
+# and the full stop should be there then a space
+#3 - python manage.py startapp 'application'    # - creats a part of the app eg users, accounts login etc
+#4 - python manage.py runserver
+
+#to kill the server  - Quit the server with CTRL-BREAK or  CTRL-C
